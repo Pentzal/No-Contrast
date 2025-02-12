10b71585d3ed838604fb4c3dd7363517352fa5d4
Pentzal <23644703@stu.mmu.ac.uk>
2025-02-12 02:53:08 +0000

    Added Suit Colors
    Suit text color now changes dynamically... to red or black!

--- NoContrast.lua
+++ NoContrast.lua
@@ -3,12 +3,15 @@
 --- MOD_ID: NoCon
 --- PREFIX: no
 --- MOD_AUTHOR: [Pentzal]
---- MOD_DESCRIPTION: Adds a "No Contrast" option to all suits. It is not recommended.
+--- MOD_DESCRIPTION: Adds a "No Contrast" option to all suits' base Deck Skins. It is not recommended.
 ----------------------------------------------
 ------------MOD CODE -------------------------
 local suits = { 'Hearts', 'Diamonds', 'Clubs', 'Spades', 'bunc_Fleurons', 'bunc_Halberds' }
 local fullRanks = {"Ace", "King", "Queen", "Jack", "10", "9", "8", "7", "6", "5", "4", "3", "2"}
 local face = {"King", "Queen", "Jack"}
+local red = HEX("fd5f55")
+local black = HEX("39494c")
+local suitColor = { red, red, black, black, red, black }
 
 SMODS.Atlas{ key = 'ContrastCards', px = 71, py = 95, path = 'NoContrast.png' }
 SMODS.Atlas{ key = 'ContrastUI', px = 18, py = 18, path = 'NoConUI.png' }
@@ -25,6 +28,7 @@ for _, suit in ipairs(suits) do
         loc_txt = {
             ['en-us'] = "No Contrast Colors"
         },
+        colour = suitColor[_],
         suit_icon = {
             atlas = (_ < 5 and 'no_ContrastUI') or (_ > 4 and 'no_ContrastUI_exotic'),
             pos = { x = ((_ - 1) % 4), y = 0 }
